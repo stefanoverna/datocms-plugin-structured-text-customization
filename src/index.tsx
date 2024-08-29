a4c7f804b4ef7d2253073175188467022cb01004
Stefano Verna <s.verna@datocms.com>
2024-08-29 09:29:19 +0200

    wip

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,38 @@
-import { connect } from 'datocms-plugin-sdk';
-import 'datocms-react-ui/styles.css';
-import ConfigScreen from './entrypoints/ConfigScreen';
-import { render } from './utils/render';
+import { connect, type Field, type FieldIntentCtx } from "datocms-plugin-sdk";
+import "datocms-react-ui/styles.css";
+import ConfigScreen from "./entrypoints/ConfigScreen";
+import { render } from "./utils/render";
 
 connect({
-  renderConfigScreen(ctx) {
-    return render(<ConfigScreen ctx={ctx} />);
-  },
+	renderConfigScreen(ctx) {
+		return render(<ConfigScreen ctx={ctx} />);
+	},
+	customBlockStylesForStructuredTextField(field: Field, ctx: FieldIntentCtx) {
+		return [
+			{
+				id: "emphasized",
+				node: "paragraph",
+				label: "Emphasized",
+				appliedStyle: {
+					fontFamily: "Georgia",
+					fontStyle: "italic",
+					fontSize: "1.4em",
+					lineHeight: "1.2",
+				},
+			},
+		];
+	},
+	customMarksForStructuredTextField(field: Field, ctx: FieldIntentCtx) {
+		return [
+			{
+				id: "spoiler",
+				label: "Spoiler",
+				icon: "bomb",
+				keyboardShortcut: "mod+shift+l",
+				appliedStyle: {
+					backgroundColor: "rgba(255, 0, 0, 0.3)",
+				},
+			},
+		];
+	},
 });
